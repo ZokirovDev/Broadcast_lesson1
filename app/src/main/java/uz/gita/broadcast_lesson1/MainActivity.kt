5f7c14fb2db51cc726a6bf4644b7024759ee7b23
shukrullo <axborotxavfsizligi@gmail.com>
2024-10-22 15:39:12 +0500

    first commit

--- app/src/main/java/uz/gita/broadcast_lesson1/MainActivity.kt
+++ app/src/main/java/uz/gita/broadcast_lesson1/MainActivity.kt
@@ -1,5 +1,7 @@
 package uz.gita.broadcast_lesson1
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,16 +12,55 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import uz.gita.broadcast_lesson1.presentation.receivers.ContextRegisteredReceiver
 import uz.gita.broadcast_lesson1.ui.theme.Broadcast_lesson1Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        //SmsReveceiverBroadcast ishlashi uchun sms ga permission olinishi kk edi. Vaqtdan yutish uchun yozilmadi. Agar siz ushbu codeni ishlatmoqchi bo`lsangiz,
-        //permissionni qo`lda berib qo`ying!
         setContent {
+            //Contextda broadcastlarni registratsiya qilish uchun quidagi obyektlarni yaratib olamiz
+
+
+            //Broadcast obyekti. Bu o`zimiz yaratib olgan class obyekti
+            val broadcast = ContextRegisteredReceiver()
+            //filter- bu qaysi actionlarni uchlashini bildiradi
+            val actionPowerConnected = IntentFilter(Intent.ACTION_POWER_CONNECTED)
+            val actionPowerDisconnected = IntentFilter(Intent.ACTION_POWER_DISCONNECTED)
+            val actionScreenOn = IntentFilter(Intent.ACTION_SCREEN_ON)
+            val actionScreenOff = IntentFilter(Intent.ACTION_SCREEN_OFF)
+            //receiverFlag - bu agar broadcast boshqa applar orqali kelish yoki kelmasligini bildiradi
+            val receiverFlag = ContextCompat.RECEIVER_EXPORTED
+
+            ContextCompat.registerReceiver(
+                LocalContext.current,
+                broadcast,
+                actionPowerConnected,
+                receiverFlag
+            )
+            ContextCompat.registerReceiver(
+                LocalContext.current,
+                broadcast,
+                actionPowerDisconnected,
+                receiverFlag
+            )
+            ContextCompat.registerReceiver(
+                LocalContext.current,
+                broadcast,
+                actionScreenOn,
+                receiverFlag
+            )
+            ContextCompat.registerReceiver(
+                LocalContext.current,
+                broadcast,
+                actionScreenOff,
+                receiverFlag
+            )
+
             Broadcast_lesson1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
---
+++ app/src/main/java/uz/gita/broadcast_lesson1/presentation/receivers/ContextRegisteredReceiver.kt
@@ -0,0 +1,40 @@
+package uz.gita.broadcast_lesson1.presentation.receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.widget.Toast
+import java.util.Locale
+
+class ContextRegisteredReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when(intent?.action){
+            Intent.ACTION_POWER_CONNECTED-> speakText(context,"Power connected")
+            Intent.ACTION_POWER_DISCONNECTED-> speakText(context,"Power disconnected")
+            Intent.ACTION_SCREEN_ON-> speakText(context,"Screen on")
+            Intent.ACTION_SCREEN_OFF-> speakText(context,"Screen off")
+        }
+
+    }
+
+
+    private fun speakText(context: Context?, text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+        var tts: TextToSpeech? = null
+        val textToSpeech = TextToSpeech(context) { status ->
+            Log.d("TTT", "getTextToSpeech: $status")
+            if (status == TextToSpeech.SUCCESS) {
+                tts?.language = Locale.US
+
+                tts?.speak(text, TextToSpeech.QUEUE_ADD, null, "")
+
+            }
+        }
+        tts = textToSpeech
+
+    }
+
+}
+
